94bf6a271addeadc45c3e14242d795a299a34dff
Adedoyin Adedara <adedoyin.adedara@gmail.com>
2026-06-03 21:17:08 +0100

    Added the description to the FastAPI()

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="ChopNow-API",
+    description="Production-ready, asynchronous food delivery backend gateway",
+    version="1.0.0"
+)
 
 @app.get("/") #the route for the home/root path
 async def read_root():
-    return {"message": "Hello World"}
+    return {"status": "healthy",
+        "service": "ChopNow-API Core Engine",
+        "docs_url": "/docs"
+            }
 
 @app.get("/greet/{name}")
 async def greet(name: str) -> dict:
