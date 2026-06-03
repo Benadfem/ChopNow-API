fc512901fcdb48049d3c6e146e4c9fe4f4a3e7ba
Adedoyin Adedara <adedoyin.adedara@gmail.com>
2026-06-03 15:15:55 +0100

    feat: add dynamic root greeting endpoint with input mutation

--- src/main.py
+++ src/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/") #the route for the home/root path
 async def read_root():
     return {"message": "Hello World"}
+
+@app.get("/greet/{name}")
+async def greet(name: str) -> dict:
+    return {"message": f"Hello, {name.upper()}!"}
