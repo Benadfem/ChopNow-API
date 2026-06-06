39bdf4bb009b6b3b9ebdba776f0de00e728b6579
Adedoyin Adedara <adedoyin.adedara@gmail.com>
2026-06-06 19:44:30 +0100

    Added the pydantic functionality to validate input given to the server

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI(
     title="ChopNow-API",
@@ -10,7 +11,7 @@ app = FastAPI(
 @app.get("/")
 async def root():
     return {
-        "message": "Hello World",
+        "message": "Hello Welcome to ChopNow-API",
     }
 
 @app.get("/greet/")
@@ -18,4 +19,22 @@ async def greet( name: Optional[str] = "User" ,title : str ="George") -> dict:
     return {
         "message": f"Hello {name}!",
         "title": title,
+    }
+# let's create the model for the user through pydantic
+class User(BaseModel):
+    name: str
+    age: int
+    role: str
+    is_active: bool
+
+
+# let's create a user for the app
+@app.post("/user")
+# you create a variable of the User type
+async def create_user(user: User):
+    return {
+        "name": user.name,
+        "age": user.age,
+        "role": user.role,
+        "is_active": user.is_active
     }
