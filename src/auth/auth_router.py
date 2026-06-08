0ad6cf13b09ca5da0181e382428ab6915502befc
Adedoyin Adedara <adedoyin.adedara@gmail.com>
2026-06-08 21:38:17 +0100

    Added a Login for the user authenticator but at this time the database is not yet created so we returned a plain dictionary just to give a response
    this is done by using the post post request method

--- src/auth/auth_router.py
+++ src/auth/auth_router.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, status
-from src.auth.schemas import UserCreate, UserResponse  # Clean relative import
+from fastapi import APIRouter, status, dependencies
+from src.auth.schemas import UserCreate, UserResponse,UserLogin  # Clean relative import
 
 # We define the router with a prefix so all routes inside automatically start with /auth
 router = APIRouter(
@@ -16,4 +16,17 @@ async def register_user(user_in: UserCreate):
         "full_name": user_in.full_name,
         "role": user_in.role,
         "is_active": True
+    }
+
+
+
+
+
+@router.post("/login", status_code=status.HTTP_200_OK)
+async def login_user(credentials: UserLogin):
+    # Simulated check: In Phase 3, we will verify the password hash and issue a JWT
+    return {
+        "access_token": "simulated_secure_jwt_token_string",
+        "token_type": "bearer",
+        "message": f"Successfully logged in as {credentials.email}"
     }
--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -21,3 +21,8 @@ class UserResponse(BaseModel):
 
     class Config:
         from_attributes = True
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str = Field(..., min_length=8)
